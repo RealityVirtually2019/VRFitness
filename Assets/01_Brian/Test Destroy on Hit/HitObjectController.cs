66a8058113486fc4d890dd7e56fbd6f77671d038
Lucid Dreams VR <luciddreamsvr@gmail.com>
2019-01-18 13:45:17 -0500

    Integrate obstacle avoidance

---
+++ Assets/01_Brian/Test Destroy on Hit/HitObjectController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class HitObjectController : MonoBehaviour
+{
+
+    public ViveRoleProperty objectRole;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        VivePoseTracker tracker = other.gameObject.transform.parent.gameObject.transform.parent.gameObject.GetComponent<VivePoseTracker>();
+        
+
+        Debug.Log("Object role: " + objectRole + " ,Tracker role: " + tracker.viveRole);
+
+        if (objectRole == tracker.viveRole)
+        {
+            Destroy(gameObject);
+            Debug.Log("correct entered!");
+        } 
+
+        //Debug.Log(tracker.viveRole + " entered");
+    }
+
+
+}
