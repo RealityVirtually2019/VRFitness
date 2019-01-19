b07ddff3a5ec2f2e9a0ae0b8a13039af130629e7
kpalmer <daemencompany@gmail.com>
2019-01-19 17:21:41 -0500

    Created screen overlay for when user makes wrong move

---
+++ Assets/01_Kristen/FollowUser/ScreenOverlay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenOverlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       this.gameObject.GetComponent<Renderer>().material.color = new Color(255.0f,0.0f,0.0f,0.5f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
