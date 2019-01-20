1f136cad9d218f564d1981e64bacb16627c318fe
Lucid Dreams VR <luciddreamsvr@gmail.com>
2019-01-19 23:31:30 -0500

    Added Menu to Main Scene (Still needs work) and Added TextMesh Pro

--- Assets/00_VRFitness/Scripts/StartExercise.cs
+++ Assets/00_VRFitness/Scripts/StartExercise.cs
@@ -6,28 +6,46 @@ public class StartExercise : MonoBehaviour
 {
 
     public GameObject exerciseTimeline;
-    public GameObject text;
+    public GameObject mesh;
     public GameObject otherTrigger;
-    public GameObject textOther;
 
-    private float startSpeed;
-    private float endSpeed;
+    private float speed = 0;
+    private float endTime = float.PositiveInfinity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        exerciseTimeline.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mesh.transform.Rotate(Vector3.up, Time.deltaTime * speed);
+
+        if (Time.time >= endTime)
+        {
+            StartExerciseTimeline();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        endTime = Time.time + 3.0f;
+        speed = 140f;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        endTime = float.PositiveInfinity;
+        speed = 0;
+    }
+
+    private void StartExerciseTimeline()
+    {
+        exerciseTimeline.SetActive(true);
+        Destroy(mesh);
+        Destroy(gameObject);
     }
 
 }
