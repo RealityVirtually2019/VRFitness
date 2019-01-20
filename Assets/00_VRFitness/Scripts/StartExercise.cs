2891e7b0d616d64f92e45388f4f83297c96408f5
Lucid Dreams VR <luciddreamsvr@gmail.com>
2019-01-19 22:24:16 -0500

    Changed scene Name to Main Demo

---
+++ Assets/00_VRFitness/Scripts/StartExercise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartExercise : MonoBehaviour
+{
+
+    public GameObject exerciseTimeline;
+    public GameObject text;
+    public GameObject otherTrigger;
+    public GameObject textOther;
+
+    private float startSpeed;
+    private float endSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
