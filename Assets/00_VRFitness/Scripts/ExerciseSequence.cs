88f7199219bb460080f00383bde22b3a579e1917
Lucid Dreams VR <luciddreamsvr@gmail.com>
2019-01-19 19:29:32 -0500

    Add more paths, use path test to for ExerciseSequences

--- Assets/00_VRFitness/Scripts/ExerciseSequence.cs
+++ Assets/00_VRFitness/Scripts/ExerciseSequence.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathTester : MonoBehaviour
+public class ExerciseSequence : MonoBehaviour
 {
 	public GameObject[] PathPrefabs;
 	public float SecondsBetweenStarting = 1.0f;
